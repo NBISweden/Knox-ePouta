1cde39c41cb53662b1812be85b5c99235daf9b64
Frédéric Haziza <daz@bils.se>
2016-05-09 11:33:04 +0200

    Correcting Bash syntax

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # mosler-micro-mosler
 
-Requirements: Centos6 glance image installed
-
-Clone of 
+Requirements:
+* Centos6 glance image installed
+* Clone of 
 git@github.com:BILS/mosler-system-scripts.git
 in $HOME
+* Thinlinc packages in $HOME/thinlinc
 
-Thinlinc packages in $HOME/thinlinc
+You also need to create a file 'user.rc' that will set up 2 variables:
+```export OS_USERNAME=<username>
+export OS_PASSWORD=<password>```
--- settings.sh
+++ settings.sh
@@ -21,15 +21,15 @@ TENANT_ID=$(openstack project list | awk '/'${OS_TENANT_NAME}'/ {print $2}')
 # Declaring the machines names
 
 declare -a MACHINES
-MACHINES=(
-    (openstack-controller 3 'm1.small') 
-    (thinlinc-master      4 'm1.small')
-    (filsluss             5 'm1.small')
-    (supernode            6 'm1.small')
-    (compute1             7 'm1.large')
-    (compute2             8 'm1.large' 111)
-    (compute2             9 'm1.large' 112)
-    (hnas-emulation      10 'm1.small')
-    (ldap                11 'm1.small')
-    (networking-node     12 'm1.small' 101)
-    )
+MACHINES=(\
+    (openstack-controller 3 'm1.small') \
+    (thinlinc-master      4 'm1.small') \
+    (filsluss             5 'm1.small') \
+    (supernode            6 'm1.small') \
+    (compute1             7 'm1.large') \
+    (compute2             8 'm1.large' 111) \
+    (compute2             9 'm1.large' 112) \
+    (hnas-emulation      10 'm1.small') \
+    (ldap                11 'm1.small') \
+    (networking-node     12 'm1.small' 101) \
+)
