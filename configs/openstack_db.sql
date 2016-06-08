d4168a6aa8045a051ab014d6b5889b46225b55f1
Frédéric Haziza <daz@bils.se>
2016-06-08 17:07:11 +0200

    MySQL installation is now working and idempotent

--- configs/openstack_db.sql
+++ configs/openstack_db.sql
@@ -1,14 +1,23 @@
 DELETE FROM mysql.user WHERE User='';
-#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+-- UPDATE mysql.user SET Password='' WHERE User='root';
+-- UPDATE mysql.user SET Password=PASSWORD('mysql') WHERE User='root';
+
+-- CREATE USER 'root'@'openstack-controller' IDENTIFIED BY 'mysql';
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED by 'mysql' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'openstack-controller' IDENTIFIED by 'mysql' WITH GRANT OPTION;
+
 FLUSH PRIVILEGES;
 
-# DROP DATABASE IF EXISTS nova;
-# DROP DATABASE IF EXISTS neutron;
-# DROP DATABASE IF EXISTS keystone;
-# DROP DATABASE IF EXISTS glance;
-# DROP DATABASE IF EXISTS heat;
+--  DROP DATABASE IF EXISTS nova;
+--  DROP DATABASE IF EXISTS neutron;
+--  DROP DATABASE IF EXISTS keystone;
+--  DROP DATABASE IF EXISTS glance;
+--  DROP DATABASE IF EXISTS heat;
 
 CREATE DATABASE nova;
 CREATE DATABASE keystone;
@@ -16,18 +25,18 @@ CREATE DATABASE glance;
 CREATE DATABASE neutron;
 CREATE DATABASE heat;
 
-GRANT ALL PRIVILEGES ON nova.* to nova@localhost identified by 'nova';
-GRANT ALL PRIVILEGES ON nova.* to nova@'%' identified by 'nova';
+GRANT ALL PRIVILEGES ON nova.* to nova@localhost IDENTIFIED by 'nova';
+GRANT ALL PRIVILEGES ON nova.* to nova@'%' IDENTIFIED by 'nova';
 
-GRANT ALL PRIVILEGES ON neutron.* to neutron@localhost  identified by 'neutron';
-GRANT ALL PRIVILEGES ON neutron.* to neutron@'%' identified by 'neutron';
+GRANT ALL PRIVILEGES ON neutron.* to neutron@localhost IDENTIFIED by 'neutron';
+GRANT ALL PRIVILEGES ON neutron.* to neutron@'%' IDENTIFIED by 'neutron';
 
-GRANT ALL PRIVILEGES ON keystone.* to keystone@localhost identified by 'keystone';
-GRANT ALL PRIVILEGES ON keystone.* to keystone@'%' identified by 'keystone';
+GRANT ALL PRIVILEGES ON keystone.* to keystone@localhost IDENTIFIED by 'keystone';
+GRANT ALL PRIVILEGES ON keystone.* to keystone@'%' IDENTIFIED by 'keystone';
 
-GRANT ALL PRIVILEGES ON glance.* to glance@localhost identified by 'glance';
-GRANT ALL PRIVILEGES ON glance.* to glance@'%' identified by 'glance';
+GRANT ALL PRIVILEGES ON glance.* to glance@localhost IDENTIFIED by 'glance';
+GRANT ALL PRIVILEGES ON glance.* to glance@'%' IDENTIFIED by 'glance';
       
-GRANT ALL PRIVILEGES ON heat.* to heat@localhost identified by 'heat';
-GRANT ALL PRIVILEGES ON heat.* to heat@'%' identified by 'heat';
+GRANT ALL PRIVILEGES ON heat.* to heat@localhost IDENTIFIED by 'heat';
+GRANT ALL PRIVILEGES ON heat.* to heat@'%' IDENTIFIED by 'heat';
 
