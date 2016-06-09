f337a645abb745bb01197588908e1307f38f332d
Frédéric Haziza <daz@bils.se>
2016-06-09 10:16:09 +0200

    Adding a REST server for in-mem notifications

---
+++ lib/notifications.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import web
+import sys
+
+notifications = {key: {} for key in sys.argv[2:] }
+
+urls = (
+    '/show', 'show',
+    '/register/(?P<machine>.+)/(?P<task>.+)/(?P<status>.+)', 'register',
+    '/status/(?P<machine>.+)/(?P<task>.+)', 'status'
+)
+
+class show:
+    def GET(self):
+        output = ''
+        for k, v in notifications.items():
+            d = ' '.join(['[ %s | %s ]' % (key, value) for (key, value) in v.items()])
+            output += '{0:>20}: {1}\n'.format(k, d)
+        return output
+
+class register:
+    def GET(self, machine, task, status):
+        d = notifications.get(machine)
+        if d is None:
+            return 'Unknown machine %s' % machine
+        d[task] = status
+
+class status:
+    def GET(self, machine, task):
+        d = notifications.get(machine)
+        if d is None:
+            return 'Unknown machine %s' % machine
+        s = d.get(task)
+        if s is None:
+            return 'Unknown task %s for %s' % (task,machine)
+        else:
+            return s
+
+if __name__ == "__main__":
+    web.config.debug = False
+    app = web.application(urls, globals())
+    app.run()
