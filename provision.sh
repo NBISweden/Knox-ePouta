575f5218c3298a35118e77ced8acda5ab92ee1e2
Frédéric Haziza <daz@bils.se>
2016-06-19 02:02:49 +0200

    Adding the NFS server configurations

--- provision.sh
+++ provision.sh
@@ -99,6 +99,7 @@ FAIL=0
 reset_progress
 print_progress
 export DB_SERVER=${MACHINE_IPs[openstack-controller]} # Used in the templates
+export NFS_SERVER=${MACHINE_IPs[hnas-emulation]} # 
 
 # set -e # exit in errors
 # trap 'print_progress; oups "\a\nErrors found: Aborting"' ERR
