70918263be214ef1503e0bc91d5182c5f6b952f9
Frederic Haziza <silverdaz@gmail.com>
2016-07-22 09:06:10 +0200

    updating the settings for the NBIS project

--- lib/supernode/files/NBIS.sh
+++ lib/supernode/files/NBIS.sh
@@ -6,9 +6,20 @@ source /root/.keystonerc
 ##############################################################
 # Create project NBIS. Users come from ldap.
 ##############################################################
-
-keystone user-role-add --tenant=tokenadmins --role=tokenadmin --user=tokenadmin1
-keystone user-role-add --tenant=tokenadmins --role=tokenadmin --user=tokenadmin2
+# -w doesn't work on nc
+function wait_port {
+    local -i t=${3:-30} # default: 30 seconds, well...if you don't count the backoff...
+    local -i backoff=1
+    local -i stride=20
+    while (( t > 0 )) ; do
+	echo -e "Time left: $t"
+	nc -4 -z -v $1 $2 && return 0
+	(( t-=backoff ))
+	sleep $backoff
+        if (( (t % stride) == 0 )); then (( backoff*=2 )); fi
+    done
+    exit 1
+}
 
 # Wait for the 3 glance images
 function wait_for_images {
