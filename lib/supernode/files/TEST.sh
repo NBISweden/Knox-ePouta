32261174c152fb52dea61b7954148c5f06e33d3e
Frederic Haziza <silverdaz@gmail.com>
2016-08-10 21:24:50 +0200

    Updating the TEST script with a 'delete button'

--- lib/supernode/files/TEST.sh
+++ lib/supernode/files/TEST.sh
@@ -2,13 +2,35 @@
 
 set -e -x
 source /root/.keystonerc
+PROJECT_NAME=TEST
 
+if [ $1 == "delete" ]; then
+    export OS_TENANT_NAME=${PROJECT_NAME}
+    nova delete ${PROJECT_NAME}-compute-node
+    nova delete ${PROJECT_NAME}-login-node
+    nova delete ${PROJECT_NAME}-service-node
+
+    neutron floatingip-list -F id | while read id; do neutron floatingip-delete $id; done
+
+    neutron router-interface-delete ${PROJECT_NAME}-router ${PROJECT_NAME}-subnet
+    neutron router-gateway-clear ${PROJECT_NAME}-router public-net
+    neutron router-delete ${PROJECT_NAME}-router
+    neutron net-delete ${PROJECT_NAME}-net
+
+    nova secgroup-delete-rule default tcp 22 22 0.0.0.0/0
+    nova secgroup-delete-rule default icmp -1 -1 0.0.0.0/0
+
+    nova keypair-delete ${PROJECT_NAME}-key
+
+    keystone tenant-delete ${PROJECT_NAME}
+fi
+
+#####################################################
 if ! glance image-show cirros &>/dev/null; then
     glance image-create --disk-format qcow2 --container-format bare --is-public True --name "cirros" --file /home/centos/vault/cirros.img 
 fi
 
-PROJECT_NAME=TEST
-
+#####################################################
 # If already exists
 keystone tenant-get ${PROJECT_NAME} &>/dev/null && echo "${PROJECT_NAME} already exists" && exit 1
 
@@ -29,9 +51,11 @@ if [ ! -e ~/ssh_key.${PROJECT_NAME} ] || [ -e ~/ssh_key.${PROJECT_NAME}.pub ]; t
 fi
 nova keypair-add --pub-key ~/ssh_key.${PROJECT_NAME}.pub ${PROJECT_NAME}-key
 
-nova secgroup-create ${PROJECT_NAME}-mosler_default "${PROJECT_NAME} secgroup"
-nova secgroup-add-rule ${PROJECT_NAME}-mosler_default tcp 22 22 0.0.0.0/0
-nova secgroup-add-rule ${PROJECT_NAME}-mosler_default icmp -1 -1 0.0.0.0/0
+# nova secgroup-create ${PROJECT_NAME}-mosler_default "${PROJECT_NAME} secgroup"
+# nova secgroup-add-rule ${PROJECT_NAME}-mosler_default tcp 22 22 0.0.0.0/0
+# nova secgroup-add-rule ${PROJECT_NAME}-mosler_default icmp -1 -1 0.0.0.0/0
+nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
+nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 
 nova boot --key-name ${PROJECT_NAME}-key --flavor mosler.1core --image cirros \
 --nic net-id=$(neutron net-list | awk '/ '${PROJECT_NAME}'-net /{print $2}') \
@@ -51,18 +75,4 @@ sleep 10
 nova floating-ip-associate ${PROJECT_NAME}-login-node $FIP
 
 # # DELETE
-# export OS_TENANT_NAME=${PROJECT_NAME}
-# nova delete ${PROJECT_NAME}-node
-
-# neutron floatingip-delete $(neutron floatingip-list | awk '/ $FIP /{print $2}')
-
-# neutron router-interface-delete ${PROJECT_NAME}-router ${PROJECT_NAME}-subnet
-# neutron router-gateway-clear ${PROJECT_NAME}-router public-net
-# neutron router-delete ${PROJECT_NAME}-router
-# neutron net-delete ${PROJECT_NAME}-net
-
-# nova secgroup-delete-rule default tcp 22 22 0.0.0.0/0
-# nova secgroup-delete-rule default icmp -1 -1 0.0.0.0/0
-
-# keystone tenant-delete ${PROJECT_NAME}
 # source /root/.keystonerc
